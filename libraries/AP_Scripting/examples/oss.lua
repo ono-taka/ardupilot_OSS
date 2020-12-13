e442c4d304c21608392d90f598012768af3a1011
ono-taka <ment@csf.ne.jp>
2020-12-14 05:39:36 +0900

    Update oss.lua

--- libraries/AP_Scripting/examples/oss.lua
+++ libraries/AP_Scripting/examples/oss.lua
@@ -36,7 +36,6 @@ local now_pitch = 0
 -- *** acrobat1 ***
 local A1_DEFDEG = 20
 local A1_DEFZSPEED = 2.5
-local a1_cnt = 0
 local A1_CNT_1 = 180
 local A1_CNT_2 = A1_CNT_1 + 180 * 2
 local A1_CNT_3 = A1_CNT_2 + 180
@@ -60,9 +59,9 @@ local A1_RADIUS = 3
 local A1_PHASE  = 4
 
 -- *** acrobat2 ***
+local a2_sub_cnt = 0
 
 -- *** acrobat3 ***
-local ac2_counter = 0
 
 -- *** acrobat4 ***
 
@@ -114,19 +113,21 @@ function update()
                 if not (v[A1_END]) then
                     if (a1_mode == "H") then
                         a1_mode = "V"
-                        a1_cnt = 0
+                        cycle_cnt = 0
                     else
+                        cycle_cnt = 0
+                        gcs:send_text(0, "[L]start acrobat2")
                         test_mode = TESTMODE_ACROBAT2
                     end
                     break
                 end
 
-                if (a1_cnt * a1_speed <= v[A1_END]) then
+                if (cycle_cnt * a1_speed <= v[A1_END]) then
                     a1_now_roll   = 0
                     a1_now_pitch  = 0
                     a1_climb_rate = 0
                     if not (v[A1_RADIUS] == 0) then
-                        deg = (a1_cnt * a1_speed - v[A1_START]) / v[A1_RADIUS] + v[A1_PHASE]
+                        deg = (cycle_cnt * a1_speed - v[A1_START]) / v[A1_RADIUS] + v[A1_PHASE]
                         if (a1_mode == "H") then
                             a1_now_roll   = A1_DEFDEG * (math.cos(math.rad(deg)))
                             a1_now_pitch  = A1_DEFDEG * (math.sin(math.rad(deg)))
@@ -146,61 +147,80 @@ function update()
                     break
                 end
             end
-            a1_cnt = a1_cnt + 1
+            
+            cycle_cnt = cycle_cnt + 1
 
         -- *** ono ***
         elseif (test_mode == TESTMODE_ACROBAT2) then
             -- control
-            -- vehicle:set_target_angle_and_climbrate(target_roll, target_pitch, target_yaw, climb_rate, use_yaw_rate, yaw_rate)
-            -- SRV_Channels:set_output_pwm_chan_timeout( 0, 1530, 1000)
-            -- SRV_Channels:set_output_pwm_chan_timeout( 1, 1535, 1000)
-            -- SRV_Channels:set_output_pwm_chan_timeout( 2, 1535, 1000)
-            -- SRV_Channels:set_output_pwm_chan_timeout( 3, 1530, 1000)
+            if (cycle_cnt < 360) then
+                a2_sub_cnt = cycle_cnt % 180
+                if (a2_sub_cnt < 45) then
+                    target_pitch = a2_sub_cnt           -- 0~44
+                    target_yaw = a2_sub_cnt * 2
+                
+                elseif (a2_sub_cnt < 90) then
+                    target_pitch = 89 - a2_sub_cnt      -- 44~0
+                    target_yaw = a2_sub_cnt * 2
+                    
+                elseif (a2_sub_cnt <135) then
+                    target_pitch = 90 - a2_sub_cnt      -- 0~-44
+                    target_yaw = 180 - ( a2_sub_cnt * 2)
+                    
+                else
+                    target_pitch = a2_sub_cnt - 179     -- -44~0
+                    target_yaw = 180 - (a2_sub_cnt * 2)
+                end
+                vehicle:set_target_angle_and_climbrate(0, target_pitch, target_yaw, 0, FALSE, 0)
             
-            -- print debug
-            -- now_yaw = math.deg(ahrs:get_yaw())
-            -- now_roll = math.deg(ahrs:get_roll())
-            -- now_pitch = math.deg(ahrs:get_pitch())
-            -- gcs:send_text(0, string.format("[L]now r:%.1f, p:%.1f, y:%.1f", now_roll, now_pitch, now_yaw))
-            
-            -- check cycle counter
-            cycle_cnt = cycle_cnt + 1
-            if (cycle_cnt > 1) then
+            elseif (cycle_cnt < 400) then
+                vehicle:set_target_angle_and_climbrate(0, 0, 0, 0, FALSE, 0)
+            else
                 cycle_cnt = 0
                 gcs:send_text(0, "[L]start acrobat3")
                 test_mode = TESTMODE_ACROBAT3
             end
             
+            -- print debug
+            now_yaw = math.deg(ahrs:get_yaw())
+            now_roll = math.deg(ahrs:get_roll())
+            now_pitch = math.deg(ahrs:get_pitch())
+            gcs:send_text(0, string.format("[L]now r:%.1f, p:%.1f, y:%.1f", now_roll, now_pitch, now_yaw))
+            
+            cycle_cnt = cycle_cnt + 1
+            
         -- *** suzuki ***
         elseif (test_mode == TESTMODE_ACROBAT3) then
 
             -- control
-            if (ac2_counter == 0) then  -- Roll left
+            if (cycle_cnt == 0) then  -- Roll left
                 gcs:send_text(0, "[L]----------Roll Left----------")
                 SRV_Channels:set_output_pwm_chan_timeout( 0, 1600, 300)    -- right    ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 1, 1300, 300)    -- left     ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 2, 1550, 300)    -- front    cw
                 SRV_Channels:set_output_pwm_chan_timeout( 3, 1550, 300)    -- rear     cw
-            elseif (ac2_counter == 40) then  -- Roll right
+            elseif (cycle_cnt == 40) then  -- Roll right
                 gcs:send_text(0, "[L]----------Roll Right----------")
                 SRV_Channels:set_output_pwm_chan_timeout( 0, 1300, 300)    -- right    ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 1, 1600, 300)    -- left     ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 2, 1550, 300)    -- front    cw
                 SRV_Channels:set_output_pwm_chan_timeout( 3, 1550, 300)    -- rear     cw
-            elseif (ac2_counter == 80) then
+            elseif (cycle_cnt == 80) then
                 gcs:send_text(0, "[L]----------Backflip----------")
                 SRV_Channels:set_output_pwm_chan_timeout( 0, 1550, 300)    -- right    ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 1, 1550, 300)    -- left     ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 2, 1600, 300)    -- front    cw
                 SRV_Channels:set_output_pwm_chan_timeout( 3, 1300, 300)    -- rear     cw
-            elseif (ac2_counter == 120) then
+            elseif (cycle_cnt == 120) then
                 gcs:send_text(0, "[L]----------Forward Rotation----------")
                 SRV_Channels:set_output_pwm_chan_timeout( 0, 1550, 300)    -- right    ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 1, 1550, 300)    -- left     ccw
                 SRV_Channels:set_output_pwm_chan_timeout( 2, 1300, 300)    -- front    cw
                 SRV_Channels:set_output_pwm_chan_timeout( 3, 1600, 300)    -- rear     cw
-            elseif (ac2_counter > 160) then
-                cycle_cnt = cycle_cnt + 1
+            elseif (cycle_cnt > 160) then
+                cycle_cnt = 0
+                gcs:send_text(0, "[L]start acrobat4")
+                test_mode = TESTMODE_ACROBAT4
             end
             
             -- print debug
@@ -209,16 +229,7 @@ function update()
             now_pitch = math.deg(ahrs:get_pitch())
             gcs:send_text(0, string.format("[L]now r:%.1f, p:%.1f, y:%.1f", now_roll, now_pitch, now_yaw))
 
-            ac2_counter = ac2_counter + 1
-
-            if (cycle_cnt > 1) then
-                --Add
-                ac2_counter = 0
-
-                cycle_cnt = 0
-                gcs:send_text(0, "[L]start acrobat4")
-                test_mode = TESTMODE_ACROBAT4
-            end
+            cycle_cnt = cycle_cnt + 1
             
         -- *** shibuya ***
         elseif (test_mode == TESTMODE_ACROBAT4) then
